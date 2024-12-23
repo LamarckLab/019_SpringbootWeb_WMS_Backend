503b1bbce5ae706604b67ac1bea7062b7bc4dbab
Lamarck <704021302@qq.com>
2024-12-23 22:33:15 +0800

    添加了MenuServiceImpl

---
+++ src/main/java/com/wms/service/impl/MenuServiceImpl.java
@@ -0,0 +1,8 @@
+package com.wms.service.impl;
+
+import com.wms.service.MenuService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MenuServiceImpl implements MenuService {
+}
